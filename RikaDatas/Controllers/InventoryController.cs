bafce51c997f9992636a4932729ae00bd29387f6
CPU-0781\Owner <ldiaz@csyoutsourcing.com>
2024-12-17 17:26:43 +0800

    Release 4.1 - Added Execution using only 1 URL

---
+++ RikaDatas/Controllers/InventoryController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RikaDatas.Controllers
+{
+    public class InventoryController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string text)
+        {
+
+            string apiUrl = text;
+            Console.WriteLine($"Executing: {apiUrl}");
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(apiUrl);
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode && jsonResponse.Contains("Proceeded"))
+                {
+                    Console.WriteLine("Execution Finished...");
+                    ViewBag.Message = "Execution Finished";
+                }
+                else
+                {
+                    Console.WriteLine("Error...");
+                    ViewBag.Message = "Error";
+                }
+            }
+            
+            return View();
+        }
+
+    }
+}
